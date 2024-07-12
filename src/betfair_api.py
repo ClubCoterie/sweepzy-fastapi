fe0b84c6c77b97fc808cab2f696dbfd71f14bebf
declanmcgann <dec17007@gmail.com>
2024-07-12 13:08:25 +0200

    Initialise

--- src/betfair_api.py
+++ src/betfair_api.py
@@ -1,12 +1,11 @@
 from dotenv import load_dotenv
 import urllib.error, urllib.request
 import betfairlightweight
-from betfairlightweight import filters
 import pandas as pd
-import numpy as np
 import os
 import datetime
 import json
+pd.set_option('display.max_rows', None)
 
 class Betfair:
 
@@ -25,6 +24,24 @@ class Betfair:
         sport_filter = betfairlightweight.filters.market_filter(text_query=sport)
         sport_event_type = self.trading.betting.list_event_types(filter=sport_filter)
         self.event_type_id = sport_event_type[0].event_type.id
+
+    def list_all_events_found(self, datetime_p):
+        event_filter = betfairlightweight.filters.market_filter(
+            event_type_ids=[self.event_type_id],
+            market_start_time={
+                'to': datetime_p
+            })
+
+        events = self.trading.betting.list_events(
+            filter=event_filter
+        )
+
+        events_df = pd.DataFrame({
+            'Event Name': [event_object.event.name for event_object in events],
+            'Event ID': [event_object.event.id for event_object in events],
+            'Open Date': [event_object.event.open_date for event_object in events]
+        })
+        print(events_df)
 
     def get_event_id(self, datetime_p, event_name):
         event_filter = betfairlightweight.filters.market_filter(
@@ -76,16 +93,14 @@ class Betfair:
         statuses = [runner_book.status for runner_book in runner_books]
 
         df = pd.DataFrame({
-            'Selection ID': selection_ids,
-            'Best Back Price': best_back_prices,
+            'selectionId': selection_ids,
+            'Best_Back_Price': best_back_prices,
             'Status': statuses
         })
         return df
 
     def get_runners_and_prices(self):
-        price_filter = betfairlightweight.filters.price_projection(
-            price_data=['EX_BEST_OFFERS']
-        )
+        price_filter = betfairlightweight.filters.price_projection(price_data=['EX_BEST_OFFERS'])
 
         # Request market books
         market_books = self.trading.betting.list_market_book(
@@ -98,7 +113,7 @@ class Betfair:
 
         runners_df = self.process_runner_books(market_book.runners)
 
-        print(runners_df)
+        return runners_df.loc[runners_df["Status"]=="ACTIVE", ["selectionId", "Best_Back_Price"]]
 
     def callAping(self, jsonrpc_req):
         url = "https://api.betfair.com/exchange/betting/json-rpc/v1"
@@ -115,26 +130,31 @@ class Betfair:
             exit()
 
     def get_runner_names(self):
-        '''Betfair light version doesnt have runner names so we neeed to make full call from raw API and join to runner df'''
+        '''Betfair light version doesnt have runner names so we neeed to make full call from raw API
+        and join to runner df'''
         now = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
         market_catalogue_req = '{"jsonrpc": "2.0", "method": "SportsAPING/v1.0/listMarketCatalogue", "params": {"filter":{"eventIds":["' + self.event_id + '"],' \
                                                                                                                                                              '"marketStartTime":{"from":"' + now + '"}},"sort":"FIRST_TO_START","maxResults":"10","marketProjection":["RUNNER_DESCRIPTION"]}}'
-        '''Line above has 2 lines so dont break.'''
+        '''Line above has 2 lines so don't break.
+        Using event_id and now datetime to show markets now available '''
         market_catalogue_response = self.callAping(market_catalogue_req)
         market_catalouge_loads = json.loads(market_catalogue_response)
-        market_catalouge_results = market_catalouge_loads['result']
-        runners_list = market_catalouge_results[0]['runners']
-        print(runners_list)
+        runners_list = market_catalouge_loads['result'][0]['runners']
         df = pd.DataFrame(runners_list, columns=['selectionId', 'runnerName'])
-        print(df)
 
-if __name__ == '__main__':
-    betfair = Betfair()
-    betfair.get_event_type_id("Golf")
-    datetime_in_a_week = (datetime.datetime.utcnow() + datetime.timedelta(weeks=2)).strftime("%Y-%m-%dT%TZ")
-    betfair.get_event_id(datetime_in_a_week, "Open Championship 2024")
-    betfair.get_market_id("Winner")
-    # betfair.get_runners_and_prices()
-    betfair.get_runner_names()
+        return df
+
+    def get_runners_df(self, sport, event_name, datetime_in_a_week):
+        self.get_event_type_id(sport)
+        self.get_event_id(datetime_in_a_week, event_name)
+        self.get_market_id("Winner")
+        df = self.get_runners_and_prices()
+        df2 = self.get_runner_names()
+        df_out = df.merge(df2, on="selectionId", how="left")
+        df_out["Rank"] = df_out["Best_Back_Price"].rank(method="first").astype(int)
+        df_out = df_out.loc[:, ["runnerName", "Rank"]].sort_values(by="Rank").reset_index(drop=True)
+        # print(df_out)
+        return df_out
+
 
 
--- src/golf_event.py
+++ src/golf_event.py
@@ -1,54 +1,66 @@
-import pandas as pd, random
+import pandas as pd, random, datetime
+from betfair_api import Betfair
 pd.set_option('display.max_rows', None)
 
 class GetData:
 
     def __init__(self):
-        '''Here I will build Betfair API to pull in data for event and sort by ranking.
-        Also get data of players from website.
-        For now using CSV of examples.'''
-        self.golfers = pd.read_csv("../data/in/golf_players.csv")
+        '''Test Data before website and Betfair provides this data.'''
+        # self.runners = pd.read_csv("../data/in/golf_players.csv")
         self.entrants = pd.read_csv("../data/in/entrants.csv")
+
+    def create_betfair_object(self):
+        self.betfair = Betfair()
 
 class Golf(GetData):
 
-    def __init__(self):
+    def __init__(self, even_draw, fair_draw):
         GetData.__init__(self)
+        GetData.create_betfair_object(self)
 
         '''Other Variables'''
-        self.even_draw = True  # Use all players to assign players with uneven players.
-        self.fair_draw = False  # Use odds ranking to create fair pots.
+        self.even_draw = even_draw  # Use all players to assign players with uneven players.
+        self.fair_draw = fair_draw  # Use odds ranking to create fair pots.
+
+    def list_all_events_found(self, datetime_p):
+        self.betfair.list_all_events_found(datetime_p)
+
+    def get_runner_data(self, event_name, datetime_p):
+        self.runners = self.betfair.get_runners_df("Golf", event_name, datetime_p)
 
     def add_pot(self):
         '''Add Pot number to Player Dataframe based on number entrants'''
         pot = 1
-        for i in range(len(self.entrants), len(self.golfers), len(self.entrants)):
-            self.golfers.loc[i - len(self.entrants):i-1, ["Pot"]] = pot
+        for i in range(len(self.entrants), len(self.runners), len(self.entrants)):
+            print(i)
+            print(len(self.entrants))
+            self.runners.loc[i - len(self.entrants):i-1, ["Pot"]] = pot
             pot+=1
         if self.even_draw == True:
-            self.golfers.dropna(inplace=True)
+            self.runners.dropna(inplace=True)
         if self.even_draw == False:
-            modulo = len(self.golfers) % len(self.entrants)
+            modulo = len(self.runners) % len(self.entrants)
             if modulo > 0:
-                last_rank = self.golfers['Rank'].iloc[-1]
-                add_rows = pd.DataFrame([{"Players":"None Asigned", "Rank":last_rank+i, "Pot":None} for i in range(1, len(self.entrants)+1-modulo)])
-                self.golfers = pd.concat([self.golfers, add_rows], ignore_index=True)
-            self.golfers.loc[self.golfers["Pot"].isna(), "Pot"] = pot
-        self.golfers["Pot"] = self.golfers["Pot"].astype(int)
+                last_rank = self.runners['Rank'].iloc[-1]
+                add_rows = pd.DataFrame([{"runnerName":"None Asigned", "Rank":last_rank+i, "Pot":None} for i in range(1, len(self.entrants)+1-modulo)])
+                self.runners = pd.concat([self.runners, add_rows], ignore_index=True)
+            self.runners.loc[self.runners["Pot"].isna(), "Pot"] = pot
+        self.runners["Pot"] = self.runners["Pot"].astype(int)
+        print(self.runners)
 
     def run_draw(self):
-
+        self.add_pot()
         if self.fair_draw == False:
-            rank_range = self.golfers.loc[:, "Rank"].tolist()
-        for pot in self.golfers["Pot"].unique():
+            rank_range = self.runners.loc[:, "Rank"].tolist()
+        for pot in self.runners["Pot"].unique():
             print(f"Drawing Pot {pot}")
             self.entrants[f"Pot_{pot}"] = ""  # Create empty pot col to store player name.
             if self.fair_draw == True:
-                rank_range = self.golfers.loc[self.golfers["Pot"] == pot, "Rank"].tolist()
-            # print(rank_range)
+                rank_range = self.runners.loc[self.runners["Pot"] == pot, "Rank"].tolist()
+            print(rank_range)
             for i in self.entrants.index:
                 random_index = random.randrange(0, len(rank_range), 1)
-                player_drawn = self.golfers.loc[self.golfers["Rank"] == rank_range[random_index], "Players"].values[0]
+                player_drawn = self.runners.loc[self.runners["Rank"] == rank_range[random_index], "runnerName"].values[0]
                 print(f"{player_drawn}")
                 self.entrants.loc[i, f"Pot_{pot}"] = player_drawn
                 rank_range.pop(random_index)
@@ -56,6 +68,13 @@ class Golf(GetData):
         self.entrants.to_csv("../data/out/draw_output.csv", index=False)
 
 if __name__ == '__main__':
-    golf = Golf()
-    golf.add_pot()
+    golf = Golf(even_draw=True, fair_draw=True)
+
+    '''datetime variable for length time to check.'''
+    datetime_in_a_week = (datetime.datetime.utcnow() + datetime.timedelta(weeks=2)).strftime("%Y-%m-%dT%TZ")
+
+    '''Use this to get event name for event'''
+    # golf.list_all_events_found(datetime_in_a_week)
+
+    golf.get_runner_data("Open Championship 2024", datetime_in_a_week)
     golf.run_draw()
